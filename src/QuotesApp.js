123ee91144890fc1d9cbce8f8bc61661e0b69db1
mehmood-ahmad <maksuperink@gmail.com>
2018-07-12 11:14:56 +0500

    create basic app structure

---
+++ src/QuotesApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './QuotesApp.scss';
+
+
+class QuotesApp extends Component {
+    render() {
+        return (
+            <div className="className"> Its Working </div>
+        );
+    }
+}
+
+export default QuotesApp;
